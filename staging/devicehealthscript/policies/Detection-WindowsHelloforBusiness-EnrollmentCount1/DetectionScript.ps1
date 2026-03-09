d73f85eb6a75f8911b3b2bf5e54632b8229e5b0b
Joe Loveless <jloveless03@pm.me>
2026-03-08 19:26:20 -0500

    whfb2

---
+++ staging/devicehealthscript/policies/Detection-WindowsHelloforBusiness-EnrollmentCount1/DetectionScript.ps1
@@ -0,0 +1,26 @@
+???<#
+{D6886603-9D2F-4EB2-B667-1971041FA96B} = WUFB PIN, NGC Credential Provider
+PIN is minimum requirement for Windows Hello for Business Enrollment.
+#> 
+
+$credentialProvider = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\Credential Providers\{D6886603-9D2F-4EB2-B667-1971041FA96B}"
+
+#Count users
+if (Test-Path -path $credentialProvider){
+$userSids = (Get-ChildItem -path $credentialProvider | Where-Object { $_.Name -match "S-1-5-21|S-1-12-1"}).name.count
+}
+
+if ($userSids -ge "10"){
+Write-Output "Not Compliant, WHFB Users Enrolled = $usersids"
+Exit 1
+}
+
+if ($userSids -lt "10"){
+Write-Output "Compliant, WHFB Users Enrolled = $usersids"
+Exit 0
+}
+if ($null -eq $userSids){
+    Write-Output "Not Compliant, No WHFB users enrolled"
+    Exit 0
+}
+
